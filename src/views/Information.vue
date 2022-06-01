47d7f6daaeac4274f2ed2f55e00a91455783cdc8
Berk Ozerdogan <97380286+Wanudell@users.noreply.github.com>
2022-06-01 12:06:09 +0300

    Information Page updated

--- src/views/Information.vue
+++ src/views/Information.vue
@@ -26,42 +26,6 @@
           </div>
         </div>
       </div>
-      <div class="col mr-6">
-        <div class="card">
-          <img
-            src="https://media-exp1.licdn.com/dms/image/C4E03AQEnpcyF2pW_8g/profile-displayphoto-shrink_200_200/0/1638793890585?e=1657756800&v=beta&t=ikh5szGoBQ4WIVRNN6EvhhdBhTS_okBx_r65jcnE3SE"
-            alt=""
-          />
-          <div class="card-body">
-            <p class="card-text">
-              1990 Ankara doğumlu. Üniversite Lisansı Fen Bilimleri Fakültesi -
-              Kimya Bölümü üzerine. Daha sonrasında kendini geliştirerek ITU
-              Kalite Mühendisliği ve Yöneticiliği eğitimi alıp yükseltti
-              kendisini. Şu anda BilgeAdam Akademi Kurumu'ndan Full Stack
-              Developer olarak eğitim görmekte ve hem program hem web üzerine
-              kodlamalar yapmaktadır.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="col mr-6">
-        <div class="card">
-          <img
-            src="https://media-exp1.licdn.com/dms/image/C4E03AQEnpcyF2pW_8g/profile-displayphoto-shrink_200_200/0/1638793890585?e=1657756800&v=beta&t=ikh5szGoBQ4WIVRNN6EvhhdBhTS_okBx_r65jcnE3SE"
-            alt=""
-          />
-          <div class="card-body">
-            <p class="card-text">
-              1990 Ankara doğumlu. Üniversite Lisansı Fen Bilimleri Fakültesi -
-              Kimya Bölümü üzerine. Daha sonrasında kendini geliştirerek ITU
-              Kalite Mühendisliği ve Yöneticiliği eğitimi alıp yükseltti
-              kendisini. Şu anda BilgeAdam Akademi Kurumu'ndan Full Stack
-              Developer olarak eğitim görmekte ve hem program hem web üzerine
-              kodlamalar yapmaktadır.
-            </p>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 </template>
