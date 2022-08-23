5e5d35cf6a973ac29d7c703b4b83352451a1e198
Berk Ozerdogan <97380286+Wanudell@users.noreply.github.com>
2022-08-23 15:58:10 +0300

    Updated Comments

--- API/myPersonelAPI/data/Personel.Entites/MainEntities/Comment.cs
+++ API/myPersonelAPI/data/Personel.Entites/MainEntities/Comment.cs
@@ -1,17 +1,30 @@
 ﻿using Personel.Common;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Personel.Entites.MainEntities
 {
+    [Table("Comments", Schema = "MainEntities")]
     public class Comment : EntityBase
     {
-        public int PostId { get; set; }
-        public User UserId { get; set; }
+        [Required]
+        public Guid PostId { get; set; }
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; }
+
+        [Required]
+        [MaxLength(250)]
         public string Post { get; set; }
+
         public DateTime SendDate { get; set; }
     }
 }
